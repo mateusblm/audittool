23b9144fdc939ad9b24f43aedd8749fa2c3c1fef
Mateus Burlamaqui Moreira <est.mateusbm@celeparsl.smb4.parana>
2025-08-25 14:08:15 -0300

    first commit

---
+++ src/main/java/io/github/mateusblm/audittool/AudittoolApplication.java
@@ -0,0 +1,13 @@
+package io.github.mateusblm.audittool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AudittoolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AudittoolApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/mateusblm/audittool/AudittoolApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.mateusblm.audittool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AudittoolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
