f972d9b377aa314ec5371f51e712b6bbbec33346
Mateus Burlamaqui Moreira <est.mateusbm@celeparsl.smb4.parana>
2025-08-25 16:02:11 -0300

    feat: Criando classe controller

---
+++ src/main/java/io/github/mateusblm/audittool/controller/AuditoriaController.java
@@ -0,0 +1,70 @@
+package io.github.mateusblm.audittool.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import io.github.mateusblm.audittool.dto.AderenciaDTO;
+import io.github.mateusblm.audittool.dto.ItemChecklistDTO;
+import io.github.mateusblm.audittool.dto.NaoConformidadeDTO;
+import io.github.mateusblm.audittool.model.ItemChecklistEntity;
+import io.github.mateusblm.audittool.model.NaoConformidadeEntity;
+import io.github.mateusblm.audittool.service.AuditoriaService;
+
+
+@RestController
+@RequestMapping("/api/auditoria")
+public class AuditoriaController {
+
+    @Autowired
+    private AuditoriaService auditoriaService;
+
+    @PostMapping("/checklist")
+    public ResponseEntity<ItemChecklistEntity> criarItemChecklist(@RequestBody ItemChecklistDTO dto) {
+        return ResponseEntity.ok(auditoriaService.criarItemChecklist(dto));
+    }
+
+    @GetMapping("/checklist")
+    public ResponseEntity<List<ItemChecklistEntity>> obterTodosItensChecklist() {
+        return ResponseEntity.ok(auditoriaService.obterTodosItensChecklist());
+    }
+
+    @PostMapping("/checklist/{itemChecklistId}/naoconformidade")
+    public ResponseEntity<NaoConformidadeEntity> criarNaoConformidade(
+            @PathVariable Long itemChecklistId,
+            @RequestBody NaoConformidadeDTO dto) {
+        try {
+            return ResponseEntity.ok(auditoriaService.criarNaoConformidade(itemChecklistId, dto));
+        } catch (RuntimeException e) {
+            return ResponseEntity.badRequest().build();
+        }
+    }
+    
+    @GetMapping("/naoconformidade")
+    public ResponseEntity<List<NaoConformidadeEntity>> obterTodasNaoConformidades() {
+        return ResponseEntity.ok(auditoriaService.obterTodasNaoConformidades());
+    }
+
+    @GetMapping("/aderencia")
+    public ResponseEntity<AderenciaDTO> obterAderencia() {
+        return ResponseEntity.ok(auditoriaService.calcularAderencia());
+    }
+
+    @PutMapping("/naoconformidade/{id}/resolver")
+    public ResponseEntity<NaoConformidadeEntity> resolverNaoConformidade(@PathVariable Long id) {
+        try {
+            NaoConformidadeEntity ncResolvida = auditoriaService.resolverNaoConformidade(id);
+            return ResponseEntity.ok(ncResolvida);
+        } catch (RuntimeException e) {
+            return ResponseEntity.notFound().build();
+        }
+    }
+}
